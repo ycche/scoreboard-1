17eda0c9b5eef388c0d944a88d9a595ad4a270f4
Yichen Che <yichen.che2@gmail.com>
2022-01-18 20:38:05 -0500

    Implemented basic authentication system.

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -3,28 +3,21 @@ const genPass = require('../utils/passwordUtils').genPassword
 const connection = require ("../config/db")
 const pool = connection.pool
 
-
-module.exports.manualLogin((req,res) => {
-    const {email, password} = req.body;
-    const account = await pool.query(
-        "SELECT * FROM accounts WHERE account_email = $1", [email]
-    )
-    if (account.rows.length === 0){
-        return res.status(401).json("Invalid Credential")
-    }
-
-})
-
-export const registerAccount = ((req,res) =>{
-    const user = await pool.query("SELECT * FROM users WHERE user_email = $1",[req.email])
+module.exports.registerAccount = (async (req,res) =>{
+    console.log(req.body.email)
+    console.log(req.body.password)
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1",[req.body.email])
     if (user.rows.length !== 0){
         return res.status(401).json("There already exists a user with that e-mail.")
     }
-    const hash = genPass(req.body.password)
-    const salt = hash.salt
-    const hash = hash.hash
+    const salthash = genPass(req.body.password)
+    const salt = salthash.salt
+    const hash = salthash.hash
 
-    const newUser = await pool.query("INSERT INTO users (user_email, user_salt, user_pass) VALUES ($1,$2,$3)",[req.email, salt, hash])
-    res.redirect('/login')
+    const newUser = await pool.query("INSERT INTO users (user_email, user_salt, user_pass) VALUES ($1,$2,$3)",[req.body.email, salt, hash])
+    res.redirect('/')
+})
 
+module.exports.loginSendData = (async (req,res) => {
+    res.send(req.user)
 })
